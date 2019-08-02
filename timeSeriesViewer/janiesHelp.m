9d228fdd5ae920c5581eb8d62157b25d8806dab1
ondracej <janie.ondracek@gmail.com>
2019-08-01 20:55:09 -0400

    adding code for Time Series Viewer

---
+++ timeSeriesViewer/janiesHelp.m
@@ -0,0 +1,14 @@
+
+dir = 'F:\TUM\SWR-Project\Chick-10\Ephys\2019-04-27_22-20-26'; % dir containing .continuous files
+
+%%
+dataRecordingObj = OERecordingMF(dir);
+dataRecordingObj = getFileIdentifiers(dataRecordingObj); % creates dataRecordingObject
+
+%% Run the time series viewer gui
+timeSeriesViewer(dataRecordingObj); % loads all the channels
+
+%% Extract specific snippets of data
+%[tmpV, t_ms] =dataRecordingObj.getData(chan, timeOnset_ms, dataDuration_ms);
+[tmpV, t_ms] = dataRecordingObj.getData(5,1,20000);
+
