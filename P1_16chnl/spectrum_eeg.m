0e5867c220856c3de9b1097cd2319ac561ba7589
yeganegih <39128254+yeganegih@users.noreply.github.com>
2020-10-09 17:59:49 +0200

    spectrum code to read ephys file, need to add wavelet and multitaper

--- P1_16chnl/spectrum_eeg.m
+++ P1_16chnl/spectrum_eeg.m
@@ -1,18 +1,29 @@
+addpath(genpath('D:\Part 1 PhD\Code\LabCode\LoadEphys_SWRanalysis'));
+dir_add='D:\Part 1 PhD\Data\73-03\12_03_2020'; %%%%%%%
+chnl_order=[1 2 3 4 5 6 8 7 10 9 11 12 13 16 15 14]; %%%%%%%%
+[ EEG,time,~]=OpenEphys2MAT_load_save_Data(chnl_order, '133_CH',15,dir_add); % downsample ...
+
 fs=2000;
-n=40; % number of time snippets to calculate spectrum
-t=randsample(length(EEG)-21*10^6,n)+10*10^6;
-t=sort(t);
+n=100; % number of time snippets to calculate spectrum
+% indicate the sleep time f4rom the movie
+t1=[0 6]; %%%%%% definite time of the inset of sleep
+t2=[7 40]; %%%%%%%% definite time when still being in sleep -10 min
+ind1=(t1(1)*3600+t1(2)*60)*1.5*2000; % time to index considering fs and playback ...
+% speed of movie
+ind2=(t2(1)*3600+t2(2)*60)*1.5*2000;
+ind=randsample((ind2-ind1),n)+ind1; 
+ind=sort(ind);
 clear pxx; 
 figure
 for k=1:n
-eegg=EEG(t(k)+1:t(k)+10^5,16);
+eegg=EEG(ind(k)+1:ind(k)+2000*1,16); %%%%%%% length of data for PSD and the chnl
 nwin=20000;
 [pxx(:,k),f] = pwelch(eegg,nwin,nwin/4,2^nextpow2(nwin),fs);
 plot(f,10*log10(pxx(:,k)/max(pxx(:,k)))); hold on
 xlim([0 55])
 ylabel('PSD (dB)')
 end
-title('Spectrum by pwelch (100 sampleas and medain) zf: juv w00-20'); %%%%%%%
+title('Spectrum by pwelch (100 sampleas and medain) zf: 73-03'); %%%%%%%
 m=median(pxx,2);
 plot(f,10*log10(m/max(m)),'k','linewidth',2)
 xlabel('frequency (Hz)')
